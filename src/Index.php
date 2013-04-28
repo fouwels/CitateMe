939b35abbb61dc722a0163c3d73b051edc902b80
kfouwels <kfouwels@outlook.com>
2013-04-28 16:10:57 +0100

    Nananananaaa

--- src/Index.php
+++ src/Index.php
@@ -1,37 +1,3 @@
 <?php
-
-if (isset($_GET['test']))
-{
-  print <<< EOT
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Test successful</title>
-</head>
-<body>
-<h1>Test successful</h1>
-<p>Congratulations.</p>
-<p>Apache and PHP are working.</p>
-</body>
-</html>
-EOT;
-}
-
-else
-{
-  print <<< EOT
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Test PHP</title>
-</head>
-<body>
-<h1>Test PHP</h1>
-<p>To test PHP is working correctly <a href="?test=yes">click here</a>.</p>
-</body>
-</html>
-EOT;
-}
+echo "Page loaded (it's a start)"
 ?>
