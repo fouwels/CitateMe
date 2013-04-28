4f22185a77a2f25b0816694e0ed6458ae10a7228
kfouwels <kfouwels@outlook.com>
2013-04-28 17:55:25 +0100

    I'm bored of this. Eugh PHP...

--- src/Index.php
+++ src/Index.php
@@ -1,3 +1,27 @@
+<html>
+<head>
+	<title>Citate-me Dev</title>
+	
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="row">
+		<h1>Citate Me!</h1>
+		<p><?echo "Page Loaded at: ".time()?></p>
+	</div>
+
+	<form name ="books submit" method ="post" action = "checkBook.php" id="mainForm">
+		<div class="row">
+			<INPUT type = "text" value ="Book Title">
+		</div>
+		<div class="row">
+			<INPUT type = "Submit" name = "Submit1" value = "Login">
+		</div>
+	</FORM>
+
+</body>
+</html>
+
 <?php
-echo "Page loaded (it's a start)"
 ?>
