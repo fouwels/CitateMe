053688593762d222fc83d1d607d7715d01cecb0d
kfouwels <kfouwels@outlook.com>
2013-04-28 19:46:46 +0100

    twilio libs

--- src/Index.php
+++ src/Index.php
@@ -11,17 +11,5 @@
 		<p><?echo "Page Loaded at: ".time()?></p>
 	</div>
 
-	<form name ="books submit" method ="post" action = "checkBook.php" id="mainForm">
-		<div class="row">
-			<INPUT type = "text" value ="Book Title">
-		</div>
-		<div class="row">
-			<INPUT type = "Submit" name = "Submit1" value = "Login">
-		</div>
-	</FORM>
-
 </body>
 </html>
-
-<?php
-?>
